efc0eaff7473456aa76c11f9b84399580cd9c2a8
60115 <60115@LAPTOP-TG69NOO3>
2024-04-11 15:09:10 +0800

    Add project files.

---
+++ pcd.assignment/pcd_assignment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning (disable:4996)
+
+void main() {
+	
+		printf("Hello World!");
+		return 0;
+	
+	
+
+
+	system("pause");
+}
